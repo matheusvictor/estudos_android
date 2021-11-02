5559848b5d9e06a3ff7e7fd4a8aaa539cea9e201
matheusvictor <matheusvictor.salles@gmail.com>
2021-11-01 23:36:33 -0300

    Escutando estado do botão ao clicar

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's roll!"
+
+        // ao ser clicado, o botão fará...
+        rollButton.setOnClickListener {
+            // O Toast exibe uma mensagem na tela por um curto período de tempo
+            // MainActivity é uma subclasse de Context, por isso usamos o this como parâmetro
+            // O Context permite obter informações sobre o estado atual do sistema Android
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
