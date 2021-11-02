fc0139f6e460325478e3cf63db3a63f3bb3f7d1f
matheusvictor <matheusvictor.salles@gmail.com>
2021-11-02 00:20:10 -0300

    Substituindo TextView por ImageView

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -24,18 +25,31 @@ class MainActivity : AppCompatActivity() {
 
             // Chama função para "rolar" o dado
             rollDice()
-
         }
     }
 
     // Função que exibe um número aleatório, gerado ao clicar o botão
     private fun rollDice() {
-        val resultText : TextView = findViewById(R.id.result_text)
 
         // Gerando número aleatório e armazena na variável imutável randomNumber
         // A função nextInt irá gerar um número entre 0 e 5, por isso soma-se 1
         val randomNumber = Random.nextInt(6) + 1
-        // Converte para String o número que foi gerado aleatoriamente acima
-        resultText.text = randomNumber.toString()
+
+        // drawableDice irá guardar o valor que receber da expressão when
+        val drawableDice = when (randomNumber){
+            // A expressão when irá variar a imagem do dado a ser mostrado de acordo com o número
+            // aleatório que foi gerado
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // A imagem inicial será o dado com número 1
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        // A imagem do dado irá mudar de acordo com o valor que foi obtido na expressão when
+        diceImage.setImageResource(drawableDice)
     }
 }
